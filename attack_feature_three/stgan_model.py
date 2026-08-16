224eade1d64827979bff1ad9287289cfe7fd4997
sad6648 <1546568713@qq.com>
2026-08-16 09:24:23 +0800

    Update stgan_model.py

--- attack_feature_three/stgan_model.py
+++ attack_feature_three/stgan_model.py
@@ -1,164 +1,230 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 
-class SPADE(nn.Module):
-    """SPatially-Adaptive DEnormalization."""
-    def __init__(self, num_features, label_dim):
-        super(SPADE, self).__init__()
-        self.norm = nn.BatchNorm2d(num_features, affine=False)
-        nhidden = 128
-        self.mlp_shared = nn.Sequential(
-            nn.Conv2d(label_dim, nhidden, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+class ConvGRUCell(nn.Module):
+    """GRU cell with convolutional operators. Core of the Selective Transfer Unit (STU).
+    
+    STGAN (Liu et al., CVPR 2019) uses STU to adaptively select and transform
+    encoder features for transfer to the decoder, conditioned on the attribute
+    difference vector.
+    """
+    def __init__(self, n_attrs, in_dim, out_dim, kernel_size=3):
+        super(ConvGRUCell, self).__init__()
+        self.n_attrs = n_attrs
+        self.upsample = nn.ConvTranspose2d(
+            in_dim * 2 + n_attrs, out_dim, 4, 2, 1, bias=False
         )
-        self.mlp_gamma = nn.Conv2d(nhidden, num_features, kernel_size=3, padding=1)
-        self.mlp_beta = nn.Conv2d(nhidden, num_features, kernel_size=3, padding=1)
+        self.reset_gate = nn.Sequential(
+            nn.Conv2d(in_dim + out_dim, out_dim, kernel_size, 1,
+                      (kernel_size - 1) // 2, bias=False),
+            nn.BatchNorm2d(out_dim),
+            nn.Sigmoid()
+        )
+        self.update_gate = nn.Sequential(
+            nn.Conv2d(in_dim + out_dim, out_dim, kernel_size, 1,
+                      (kernel_size - 1) // 2, bias=False),
+            nn.BatchNorm2d(out_dim),
+            nn.Sigmoid()
+        )
+        self.hidden = nn.Sequential(
+            nn.Conv2d(in_dim + out_dim, out_dim, kernel_size, 1,
+                      (kernel_size - 1) // 2, bias=False),
+            nn.BatchNorm2d(out_dim),
+            nn.Tanh()
+        )
 
-    def forward(self, x, label):
-        normalized = self.norm(x)
-        label = F.interpolate(label, size=x.size()[2:], mode='nearest')
-        actv = self.mlp_shared(label)
-        gamma = self.mlp_gamma(actv)
-        beta = self.mlp_beta(actv)
-        return normalized * (1 + gamma) + beta
-
-
-class SPADEResidualBlock(nn.Module):
-    """Residual block with SPADE normalization."""
-    def __init__(self, dim_in, dim_out, label_dim):
-        super(SPADEResidualBlock, self).__init__()
-        self.actv = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=False)
-        self.norm1 = SPADE(dim_in, label_dim)
-        self.conv2 = nn.Conv2d(dim_out, dim_out, kernel_size=3, stride=1, padding=1, bias=False)
-        self.norm2 = SPADE(dim_out, label_dim)
-        if dim_in != dim_out:
-            self.skip = nn.Conv2d(dim_in, dim_out, kernel_size=1, bias=False)
-        else:
-            self.skip = nn.Identity()
-
-    def forward(self, x, label):
-        h = self.norm1(x, label)
-        h = self.actv(h)
-        h = self.conv1(h)
-        h = self.norm2(h, label)
-        h = self.actv(h)
-        h = self.conv2(h)
-        return self.skip(x) + h
+    def forward(self, input, old_state, attr):
+        n, _, h, w = old_state.size()
+        attr = attr.view((n, self.n_attrs, 1, 1)).expand((n, self.n_attrs, h, w))
+        state_hat = self.upsample(torch.cat([old_state, attr], 1))
+        r = self.reset_gate(torch.cat([input, state_hat], dim=1))
+        z = self.update_gate(torch.cat([input, state_hat], dim=1))
+        new_state = r * state_hat
+        hidden_info = self.hidden(torch.cat([input, new_state], dim=1))
+        output = (1 - z) * state_hat + z * hidden_info
+        return output, new_state
 
 
 class STGANGenerator(nn.Module):
-    """STGAN generator with SPADE normalization."""
-    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6):
+    """STGAN Generator (Liu et al., CVPR 2019).
+    
+    Architecture: Encoder + Decoder with Selective Transfer Units (STU).
+    The STU uses a ConvGRU to selectively transfer encoder features to the
+    decoder based on the attribute difference vector (target - source).
+    This enables precise attribute editing while preserving
+    attribute-irrelevant details.
+    
+    Reference:
+      Liu et al., "STGAN: A Unified Selective Transfer Network for Arbitrary
+      Image Attribute Editing," CVPR 2019.
+    """
+    def __init__(self, conv_dim=64, c_dim=5, n_layers=5,
+                 shortcut_layers=4, stu_kernel_size=3,
+                 use_stu=True, one_more_conv=False):
         super(STGANGenerator, self).__init__()
-        self.c_dim = c_dim
-        curr_dim = conv_dim
+        self.n_attrs = c_dim
+        self.n_layers = n_layers
+        self.shortcut_layers = min(shortcut_layers, n_layers - 1)
+        self.use_stu = use_stu
 
-        # Encoder
-        self.enc0_conv = nn.Conv2d(3, curr_dim, kernel_size=7, stride=1, padding=3, bias=False)
-        self.enc0_norm = SPADE(curr_dim, c_dim)
+        # Encoder: stride-2 conv layers with BN + LeakyReLU
+        # Following Table A in the supplementary material
+        self.encoder = nn.ModuleList()
+        in_channels = 3
+        for i in range(self.n_layers):
+            out_ch = conv_dim * (2 ** i)
+            self.encoder.append(nn.Sequential(
+                nn.Conv2d(in_channels, out_ch, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(out_ch),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            ))
+            in_channels = out_ch
 
-        self.enc1_conv = nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.enc1_norm = SPADE(curr_dim * 2, c_dim)
-        curr_dim = curr_dim * 2
+        # Selective Transfer Units (one per shortcut connection)
+        if use_stu:
+            self.stu = nn.ModuleList()
+            for i in reversed(range(self.n_layers - 1 - self.shortcut_layers,
+                                     self.n_layers - 1)):
+                ch = conv_dim * (2 ** i)
+                self.stu.append(ConvGRUCell(self.n_attrs, ch, ch, stu_kernel_size))
 
-        self.enc2_conv = nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.enc2_norm = SPADE(curr_dim * 2, c_dim)
-        curr_dim = curr_dim * 2
-
-        # Bottleneck
-        self.bottleneck = nn.ModuleList()
-        for i in range(repeat_num):
-            self.bottleneck.append(SPADEResidualBlock(curr_dim, curr_dim, c_dim))
-
-        # Decoder
-        self.dec0_conv = nn.ConvTranspose2d(curr_dim, curr_dim // 2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.dec0_norm = SPADE(curr_dim // 2, c_dim)
-        curr_dim = curr_dim // 2
-
-        self.dec1_conv = nn.ConvTranspose2d(curr_dim, curr_dim // 2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.dec1_norm = SPADE(curr_dim // 2, c_dim)
-        curr_dim = curr_dim // 2
-
-        self.dec2_conv = nn.Conv2d(curr_dim, 3, kernel_size=7, stride=1, padding=3, bias=False)
-        self.dec2_tanh = nn.Tanh()
+        # Decoder: stride-2 deconv layers with skip connections
+        self.decoder = nn.ModuleList()
+        for i in range(self.n_layers):
+            if i == 0:
+                # First decoder layer: concat encoder bottleneck + attr vector
+                in_dim = conv_dim * (2 ** (self.n_layers - 1)) + self.n_attrs
+                out_dim = conv_dim * (2 ** (self.n_layers - 1))
+                self.decoder.append(nn.Sequential(
+                    nn.ConvTranspose2d(in_dim, out_dim, 4, 2, 1, bias=False),
+                    nn.BatchNorm2d(out_dim),
+                    nn.ReLU(inplace=True)
+                ))
+            elif i <= self.shortcut_layers:
+                # Shortcut layers: concat decoder output + STU output
+                in_dim = conv_dim * 3 * (2 ** (self.n_layers - 1 - i))
+                out_dim = conv_dim * (2 ** (self.n_layers - 1 - i))
+                self.decoder.append(nn.Sequential(
+                    nn.ConvTranspose2d(in_dim, out_dim, 4, 2, 1, bias=False),
+                    nn.BatchNorm2d(out_dim),
+                    nn.ReLU(inplace=True)
+                ))
+            elif i < self.n_layers - 1:
+                in_dim = conv_dim * (2 ** (self.n_layers - i))
+                out_dim = conv_dim * (2 ** (self.n_layers - 1 - i))
+                self.decoder.append(nn.Sequential(
+                    nn.ConvTranspose2d(in_dim, out_dim, 4, 2, 1, bias=False),
+                    nn.BatchNorm2d(out_dim),
+                    nn.ReLU(inplace=True)
+                ))
+            else:
+                in_dim = conv_dim * 3 if self.shortcut_layers == self.n_layers - 1 else conv_dim * 2
+                if one_more_conv:
+                    self.decoder.append(nn.Sequential(
+                        nn.ConvTranspose2d(in_dim, conv_dim // 4, 4, 2, 1, bias=False),
+                        nn.BatchNorm2d(conv_dim // 4),
+                        nn.ReLU(inplace=True),
+                        nn.ConvTranspose2d(conv_dim // 4, 3, 3, 1, 1, bias=False),
+                        nn.Tanh()
+                    ))
+                else:
+                    self.decoder.append(nn.Sequential(
+                        nn.ConvTranspose2d(in_dim, 3, 4, 2, 1, bias=False),
+                        nn.Tanh()
+                    ))
 
     def forward(self, x, c):
-        # Expand condition vector to spatial label map.
-        c = c.view(c.size(0), c.size(1), 1, 1)
-        c = c.repeat(1, 1, x.size(2), x.size(3))
-
+        """Forward pass.
+        
+        Args:
+            x: Input image [B, 3, H, W] in range [-1, 1].
+            c: Attribute difference vector [B, c_dim] (target - source).
+        Returns:
+            output: Edited image [B, 3, H, W].
+            feature_maps: List of intermediate feature maps for ATFS.
+        """
         feature_maps = []
 
-        # Encoder
-        h = self.enc0_conv(x)
-        feature_maps.append(h)
-        h = self.enc0_norm(h, c)
-        feature_maps.append(h)
-        h = F.relu(h, inplace=True)
-        feature_maps.append(h)
-
-        h = self.enc1_conv(h)
-        feature_maps.append(h)
-        h = self.enc1_norm(h, c)
-        feature_maps.append(h)
-        h = F.relu(h, inplace=True)
-        feature_maps.append(h)
-
-        h = self.enc2_conv(h)
-        feature_maps.append(h)
-        h = self.enc2_norm(h, c)
-        feature_maps.append(h)
-        h = F.relu(h, inplace=True)
-        feature_maps.append(h)
-
-        # Bottleneck
-        for block in self.bottleneck:
-            h = block(h, c)
+        # Encoder forward
+        enc_features = []
+        h = x
+        for layer in self.encoder:
+            h = layer(h)
+            enc_features.append(h)
             feature_maps.append(h)
 
-        # Decoder
-        h = self.dec0_conv(h)
-        h = self.dec0_norm(h, c)
-        h = F.relu(h, inplace=True)
-        feature_maps.append(h)
+        # Bottleneck
+        out = enc_features[-1]
 
-        h = self.dec1_conv(h)
-        h = self.dec1_norm(h, c)
-        h = F.relu(h, inplace=True)
-        feature_maps.append(h)
+        # First decoder layer: concat attribute vector with bottleneck
+        n, _, h_b, w_b = out.size()
+        attr = c.view((n, self.n_attrs, 1, 1)).expand((n, self.n_attrs, h_b, w_b))
+        out = self.decoder[0](torch.cat([out, attr], dim=1))
+        feature_maps.append(out)
 
-        h = self.dec2_conv(h)
-        h = self.dec2_tanh(h)
-        feature_maps.append(h)
+        stu_state = enc_features[-1]
 
-        return h, feature_maps
+        # Shortcut layers with STU
+        for i in range(1, self.shortcut_layers + 1):
+            if self.use_stu:
+                stu_out, stu_state = self.stu[i - 1](
+                    enc_features[-(i + 1)], stu_state, c
+                )
+                feature_maps.append(stu_out)
+                out = torch.cat([out, stu_out], dim=1)
+                out = self.decoder[i](out)
+                feature_maps.append(out)
+            else:
+                out = torch.cat([out, enc_features[-(i + 1)]], dim=1)
+                out = self.decoder[i](out)
+                feature_maps.append(out)
+
+        # Non-shortcut decoder layers
+        for i in range(self.shortcut_layers + 1, self.n_layers):
+            out = self.decoder[i](out)
+            feature_maps.append(out)
+
+        return out, feature_maps
 
 
 class STGANDiscriminator(nn.Module):
-    """PatchGAN discriminator for STGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+    """STGAN Discriminator with adversarial and attribute classification branches.
+    
+    Following Liu et al., CVPR 2019: D_adv distinguishes real vs. fake images,
+    D_att predicts the attribute vector.
+    """
+    def __init__(self, image_size=128, attr_dim=10, conv_dim=64,
+                 fc_dim=1024, n_layers=5):
         super(STGANDiscriminator, self).__init__()
         layers = []
-        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
-        layers.append(nn.LeakyReLU(0.01))
+        in_channels = 3
+        for i in range(n_layers):
+            layers.append(nn.Sequential(
+                nn.Conv2d(in_channels, conv_dim * (2 ** i), 4, 2, 1),
+                nn.InstanceNorm2d(conv_dim * (2 ** i),
+                                  affine=True, track_running_stats=True),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            ))
+            in_channels = conv_dim * (2 ** i)
 
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1))
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
-
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        self.conv = nn.Sequential(*layers)
+        feature_size = image_size // (2 ** n_layers)
+        self.fc_adv = nn.Sequential(
+            nn.Linear(conv_dim * (2 ** (n_layers - 1)) * (feature_size ** 2), fc_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(fc_dim, 1)
+        )
+        self.fc_att = nn.Sequential(
+            nn.Linear(conv_dim * (2 ** (n_layers - 1)) * (feature_size ** 2), fc_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(fc_dim, attr_dim),
+        )
 
     def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        y = self.conv(x)
+        y = y.view(y.size()[0], -1)
+        logit_adv = self.fc_adv(y)
+        logit_att = self.fc_att(y)
+        return logit_adv, logit_att
